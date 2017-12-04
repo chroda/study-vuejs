0126b24b23f4d73bde48824373563238462ccf0b
chroda <chroda@chroda.com.br>
2017-12-04 15:24:54 -0200

    21/44

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <h1>{{ title }}</h1>
-    <ninjas></ninjas>
+    <app-header></app-header>
+    <app-ninjas></app-ninjas>
+    <app-footer></app-footer>
   </div>
 </template>
 
 <script>
 
-import Ninjas from './Ninjas.vue'
+import Header from './components/Header.vue';
+import Footer from './components/Footer.vue';
+import Ninjas from './components/Ninjas.vue';
 
 export default {
   components:{
-    'ninjas': Ninjas
+    'app-header': Header,
+    'app-footer': Footer,
+    'app-ninjas': Ninjas,
   },
   data () {
     return {
-      title:'Ninja App!'
+
     }
   },
   methods:{
@@ -24,7 +29,4 @@ export default {
 </script>
 
 <style scoped>
-  h1{
-    color:purple;
-  }
 </style>
