7bc43862ad5f3ce233fa9ded0f01bbd4fc8cc0dd
chroda <chroda@chroda.com.br>
2017-12-04 11:47:17 -0200

    15/44

--- playlist/app.js
+++ playlist/app.js
@@ -1,33 +1,23 @@
-var one = new Vue({
-  el: '#vue-app-one',
-  data:{
-    title: 'Vue App One'
+Vue.component('greeting', {
+  template:'<p>Hey there, I am {{ name }} <button v-on:click="changeName()">Change name</button> </p>',
+  data:function(){
+    return {
+      name: 'Yoshi'
+    }
   },
-  methods: {
-
-  },
-  computed: {
-    greet:function(){
-      return 'Hello from app one :)';
+  methods:{
+    changeName:function(){
+      this.name = 'Mario';
     }
   }
+});
+
+var one = new Vue({
+  el: '#vue-app-one',
+
 });
 
 var two = new Vue({
   el: '#vue-app-two',
-  data:{
-    title: 'Vue App Two'
-  },
-  methods: {
-    changeTitle:function(){
-      one.title = 'Title changed';
-    }
-  },
-  computed: {
-    greet:function(){
-      return 'Yo dudes, this is app 2 speaking to ya :)';
-    }
-  }
-});
 
-two.title = "Changed from outside"
+});
