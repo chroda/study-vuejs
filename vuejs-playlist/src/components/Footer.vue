36fcac8f4c4e3921821d0196fe20cd307e966e9b
chroda <chroda@chroda.com.br>
2017-12-04 16:28:10 -0200

    23/44

--- vuejs-playlist/src/components/Footer.vue
+++ vuejs-playlist/src/components/Footer.vue
@@ -1,14 +1,19 @@
 <template>
   <footer>
-    <p>{{ copyright }}</p>
+    <p>{{ copyright }} {{ title }}</p>
   </footer>
 </template>
 
 <script>
 export default {
+  props:{
+    title:{
+      type: String,
+    }
+  },
   data () {
     return {
-      copyright:'copyright 2017 Vue Ninjas'
+      copyright:'Copyright 2017'
     }
   }
 }
