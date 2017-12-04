254bcf2da5f62a6756575a2fa225513b41f083e0
chroda <chroda@chroda.com.br>
2017-12-04 16:55:22 -0200

    26/44

--- vuejs-playlist/src/components/Header.vue
+++ vuejs-playlist/src/components/Header.vue
@@ -20,9 +20,9 @@ export default {
   },
   methods:{
     changeTitle:function(){
-      // this.$emit('changeTitle','Vue Wizards');
-      this.title = 'Vue Wizards';
-      bus.$emit('titleChanged','Vue Wizards');
+      // // this.$emit('changeTitle','Vue Wizards');
+      // this.title = 'Vue Wizards';
+      // bus.$emit('titleChanged','Vue Wizards');
     }
   }
 }
