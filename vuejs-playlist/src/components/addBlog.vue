d988ffd42406a7758f7c2fe98952b7247232cba6
chroda <chroda@chroda.com.br>
2017-12-05 21:26:14 -0200

    30/44

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -6,12 +6,26 @@
       <input type="text" v-model.lazy="blog.title" required />
       <label>Blog Content:</label>
       <textarea v-model.lazy="blog.content"></textarea>
+      <div id="checkboxes">
+        <label>Ninjas</label>
+        <input type="checkbox" value="ninjas" v-model="blog.categories">
+        <label>Wizards</label>
+        <input type="checkbox" value="wizards" v-model="blog.categories">
+        <label>Mario</label>
+        <input type="checkbox" value="mario" v-model="blog.categories">
+        <label>Cheese</label>
+        <input type="checkbox" value="cheese" v-model="blog.categories">
+      </div>
     </form>
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{blog.title}}</p>
       <p>Blog content:</p>
       <p>{{blog.content}}</p>
+      <p>Blog categories:</p>
+      <ul>
+        <li v-for="category in blog.categories">{{ category }}</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -22,7 +36,8 @@ export default {
     return {
       blog:{
         title:'',
-        content:''
+        content:'',
+        categories: []
       }
     }
   },
@@ -55,5 +70,12 @@ input[type="text"], textarea{
 }
 h3{
   margin-top:10px;
+}
+#checkboxes input{
+  display: inline-block;
+  margin-right: 10px;
+}
+#checkboxes label{
+  display: inline-block;
 }
 </style>
