93e65b5ca9d02919734b3317e7ce5f5409ea7a6b
chroda <chroda@chroda.com.br>
2017-12-05 17:51:26 -0200

    28/44

---
+++ vuejs-playlist/src/components/formOne.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form-helper>
+      <div slot="form-header">
+        <h3>Form One - Contact Us</h3>
+        <p>Fill this form to contact us</p>
+      </div>
+      <div slot="form-fields">
+        <input type="text" placeholder="name" required>
+        <label>Your Message:</label>
+        <textarea></textarea>
+      </div>
+      <div slot="form-controls">
+        <button v-on:click="handleSubmit">Send</button>
+      </div>
+    </form-helper>
+  </div>
+</template>
+<script>
+
+import formHelper from './formHelper.vue';
+
+export default {
+  components:{
+    'form-helper': formHelper,
+  },
+  data () {
+    return {
+      handleSubmit(){}
+    }
+  },
+  methods:{
+  }
+}
+</script>
+<style scoped>
+</style>
