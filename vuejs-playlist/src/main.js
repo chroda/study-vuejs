0d449d710a32b563dc087378ff2ae0d86011c1e3
chroda <chroda@chroda.com.br>
2017-12-06 20:47:38 -0200

    37/44

--- vuejs-playlist/src/main.js
+++ vuejs-playlist/src/main.js
@@ -4,15 +4,6 @@ import VueResource from 'vue-resource'
 
 Vue.use(VueResource);
 
-// Filters
-Vue.filter('to-uppercase',function(value){
-  return value.toUpperCase();
-});
-
-Vue.filter('snippet',function(value){
-  return value.slice(0,100) + '...';
-});
-
 new Vue({
   el: '#app',
   render: h => h(App)
