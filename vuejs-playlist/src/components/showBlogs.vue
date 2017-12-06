38312fbf9d99f4eeaa1e62da8ae34d0e2836e98c
chroda <chroda@chroda.com.br>
2017-12-06 15:50:59 -0200

    35/44

--- vuejs-playlist/src/components/showBlogs.vue
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -1,11 +1,9 @@
 <template>
-  <div v-theme:column="'wide'" id="show-blogs">
+  <div id="show-blogs">
     <h1>All Blog Articles</h1>
     <div v-for="blog in blogs" class="single-blog">
-      <h2 v-rainbow>{{ blog.title }}</h2>
-      <article>
-        {{ blog.body }}
-      </article>
+      <h2 v-rainbow>{{ blog.title | to-uppercase }}</h2>
+      <article>{{ blog.body | snippet }}</article>
     </div>
   </div>
 </template>
