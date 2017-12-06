f842cb73d55c9a8f47004f022333413cdc1575de
chroda <chroda@chroda.com.br>
2017-12-06 00:28:13 -0200

    32/44

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="add-blog">
     <h2>Add a New Blog Post</h2>
-    <form>
+    <form v-if="!submitted">
       <label>Blog Title:</label>
       <input type="text" v-model.lazy="blog.title" required />
       <label>Blog Content:</label>
@@ -21,7 +21,11 @@
       <select v-model="blog.author">
         <option v-for="author in authors">{{ author }}</option>
       </select>
+      <button v-on:click.prevent="post">Add Blog</button>
     </form>
+    <div v-if="submitted">
+      <h3>Thanks for adding your post</h3>
+    </div>
     <div id="preview">
       <h3>Preview Blog</h3>
       <p>Blog title: {{blog.title}}</p>
@@ -45,11 +49,21 @@ export default {
         content:'',
         categories: []
       },
-      authors:['The Net Ninja','The Angular Avenger','The Vue Vindicator']
+      authors:['The Net Ninja','The Angular Avenger','The Vue Vindicator'],
+      submitted: false,
     }
   },
   methods:{
-
+    post: function(){
+      this.$http.post('https://jsonplaceholder.typicode.com/posts',{
+        title: this.blog.title,
+        body: this.blog.content,
+        userId:1
+      }).then(function(data){
+        console.log(data);
+        this.submitted = true;
+      });
+    }
   }
 }
 </script>
