3b9fa1ad46cb54a612d52b87b95722230f616ad6
chroda <chroda@chroda.com.br>
2017-12-06 14:34:57 -0200

    33/44

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
-    <add-blog></add-blog>
+    <show-blogs></show-blogs>
   </div>
 </template>
 
 <script>
 
 import addBlog from './components/addBlog.vue';
+import showBlogs from './components/showBlogs.vue';
 
 export default {
   components:{
-    'add-blog':addBlog
+    'add-blog':addBlog,
+    'show-blogs':showBlogs
   },
   data () {
     return {
