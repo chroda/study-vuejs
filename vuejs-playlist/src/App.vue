ffa1f3b234ac1f8cfbbbf66261f891d067a3345a
chroda <chroda@chroda.com.br>
2017-12-07 11:39:50 -0200

    41/44

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <app-header></app-header>
     <router-view></router-view>
   </div>
 </template>
@@ -9,12 +10,14 @@
 import addBlog from './components/addBlog.vue';
 import showBlogs from './components/showBlogs.vue';
 import listBlogs from './components/listBlogs.vue';
+import header from './components/header.vue';
 
 export default {
   components:{
     'add-blog':addBlog,
     'show-blogs':showBlogs,
     'list-blogs':listBlogs,
+    'app-header':header,
   },
   data () {
     return {
