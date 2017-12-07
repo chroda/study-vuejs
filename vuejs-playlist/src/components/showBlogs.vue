4309f6e976f047fc83d03dbbb0659793327fbbe3
chroda <chroda@chroda.com.br>
2017-12-07 14:55:05 -0200

    44/44

--- vuejs-playlist/src/components/showBlogs.vue
+++ vuejs-playlist/src/components/showBlogs.vue
@@ -4,7 +4,7 @@
     <input type="text" v-model="search" placeholder="search blogs" />
     <div v-for="blog in filteredBlogs" class="single-blog">
       <router-link v-bind:to="'/blog/' + blog.id"><h2>{{ blog.title | to-uppercase }}</h2></router-link>
-      <article>{{ blog.body }}</article>
+      <article>{{ blog.content }}</article>
     </div>
   </div>
 </template>
@@ -22,8 +22,15 @@ export default {
 
   },
   created(){
-    this.$http.get('https://jsonplaceholder.typicode.com/posts').then(function(data){
-      this.blogs = data.body.slice(0,10);
+    this.$http.get('https://vue-playlist-d1682.firebaseio.com/posts.json').then(function(data){
+      return data.json();
+    }).then(function(data){
+      var blogsArray = [];
+      for (let key in data){
+        data[key].id = key;
+        blogsArray.push(data[key]);
+      }
+      this.blogs = blogsArray;
     });
   },
   computed:{
--- vuejs-playlist/src/components/singleBlog.vue
+++ vuejs-playlist/src/components/singleBlog.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="single-blog">
     <h1>{{ blog.title }}</h1>
-    <article>{{ blog.body }}</article>
+    <article>{{ blog.content }}</article>
+    <p>Author: {{blog.author}}</p>
+    <ul>
+      <li v-for="category in blog.categories">{{ category }}</li>
+    </ul>
   </div>
 </template>
 
@@ -17,9 +21,10 @@ export default {
     }
   },
   created() {
-    this.$http.get('http://jsonplaceholder.typicode.com/posts/' + this.id).then(function(data){
-      // console.log(data);
-      this.blog = data.body;
+    this.$http.get('https://vue-playlist-d1682.firebaseio.com/posts/' + this.id + '.json').then(function(data){
+      return data.json();
+    }).then(function(data){
+      this.blog = data;
     });
   }
 }
