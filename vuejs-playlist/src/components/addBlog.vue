837777626bfca6110ba218262b35faebc30b8f4a
chroda <chroda@chroda.com.br>
2017-12-07 14:28:36 -0200

    43/44

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -55,11 +55,7 @@ export default {
   },
   methods:{
     post: function(){
-      this.$http.post('https://jsonplaceholder.typicode.com/posts',{
-        title: this.blog.title,
-        body: this.blog.content,
-        userId:1
-      }).then(function(data){
+      this.$http.post('https://vue-playlist-d1682.firebaseio.com/posts.json',this.blog).then(function(data){
         console.log(data);
         this.submitted = true;
       });
