13512c37428796f6a8a249d4d0dc39214dabfefd
chroda <chroda@chroda.com.br>
2018-07-31 01:52:22 -0300

    behaviors

--- udemy-vuejs2/monsterSlayer/app.js
+++ udemy-vuejs2/monsterSlayer/app.js
@@ -12,19 +12,44 @@ new Vue({
             this.monsterHealth = 100;
         },
         attack(){
-            let max = 10;
-            let min = 3;
-            let damage = Math.max(Math.floor(Math.random() * max) + 1, min);
-            this.monsterHealth -= damage;
+            this.monsterHealth -= this.calculateDamage(3,10);
+            this.monsterAttach();
         },
         specialAttack(){
-
+            this.monsterHealth -= this.calculateDamage(10,20);
+            this.monsterAttach();
         },
         heal(){
-
+            if(this.playerHealth < 90){ this.playerHealth += 10; }
+            this.monsterAttach();
         },
         giveUp(){
-
+            this.gameIsRunning = false;
         },
+        monsterAttach(){
+            this.playerHealth -= this.calculateDamage(5,15);
+            if(this.checkWin()){ return; }
+        },
+        calculateDamage(min,max){
+            return damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+        },
+        checkWin(){
+            if (this.monsterHealth <= 0){
+                if (confirm('You won! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            } else if (this.playerHealth <= 0) {
+                if (confirm('You lost! New Game?')) {
+                    this.startGame();
+                } else {
+                    this.gameIsRunning = false;
+                }
+                return true;
+            }
+            return false;
+        }
     }
 });
