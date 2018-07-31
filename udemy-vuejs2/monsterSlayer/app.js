fad87703374f35df5e77d625bb18a71373bfdc92
chroda <chroda@chroda.com.br>
2018-07-31 02:16:10 -0300

    finished

--- udemy-vuejs2/monsterSlayer/app.js
+++ udemy-vuejs2/monsterSlayer/app.js
@@ -4,34 +4,55 @@ new Vue({
         playerHealth: 100,
         monsterHealth: 100,
         gameIsRunning: false,
+        turns:[]
     },
     methods:{
         startGame(){
             this.gameIsRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns=[]
         },
         attack(){
-            this.monsterHealth -= this.calculateDamage(3,10);
+            let damage = this.calculateDamage(3,10);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster for ${damage}`
+            });
             this.monsterAttach();
         },
         specialAttack(){
-            this.monsterHealth -= this.calculateDamage(10,20);
+            let damage = this.calculateDamage(15,30);
+            this.monsterHealth -= damage;
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player hits Monster hard for ${damage}`
+            });
             this.monsterAttach();
         },
         heal(){
             if(this.playerHealth < 90){ this.playerHealth += 10; }
+            this.turns.unshift({
+                isPlayer: true,
+                text: `Player heals for 10`
+            });
             this.monsterAttach();
         },
         giveUp(){
             this.gameIsRunning = false;
         },
         monsterAttach(){
-            this.playerHealth -= this.calculateDamage(5,15);
+            let damage = this.calculateDamage(5,15);
+            this.playerHealth -= damage;
+            this.turns.unshift({
+                isPlayer: false,
+                text: `Monster hits Player for ${damage}`
+            });
             if(this.checkWin()){ return; }
         },
         calculateDamage(min,max){
-            return damage = Math.max(Math.floor(Math.random() * max) + 1, min);
+            return Math.max(Math.floor(Math.random() * max) + 1, min);
         },
         checkWin(){
             if (this.monsterHealth <= 0){
