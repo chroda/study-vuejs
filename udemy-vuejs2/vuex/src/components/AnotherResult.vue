6769026d4dbc377b8896644447387f936be8221a
chroda <chroda@chroda.com.br>
2018-09-19 17:05:31 -0300

    getters explained

---
+++ udemy-vuejs2/vuex/src/components/AnotherResult.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>Counter is: {{ counter }}</p>
+    <p>Number of clicks: {{ clicks }}</p>
+  </div>
+</template>
+
+<script>
+  export default {
+    computed: {
+      counter(){
+        return this.$store.getters.doubleCounter;
+      },
+      clicks(){
+        return this.$store.getters.stringCounter;
+      }
+    }
+  }
+</script>
