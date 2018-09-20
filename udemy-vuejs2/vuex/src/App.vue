cde8afdef9188ac06c3f4ad9eb2778c4d8d4579f
chroda <chroda@chroda.com.br>
2018-09-20 10:23:48 -0300

    mutations

--- udemy-vuejs2/vuex/src/App.vue
+++ udemy-vuejs2/vuex/src/App.vue
@@ -4,9 +4,10 @@
       <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
         <h1>Vuex</h1>
         <app-result></app-result>
-        <app-another-result></app-another-result>
-        <hr>
         <app-counter></app-counter>
+        <hr>
+        <app-another-result></app-another-result>
+        <app-another-counter></app-another-counter>
       </div>
     </div>
   </div>
@@ -14,12 +15,14 @@
 
 <script>
   import Counter from './components/Counter.vue';
+  import AnotherCounter from './components/AnotherCounter.vue';
   import Result from './components/Result.vue';
   import AnotherResult from './components/AnotherResult.vue';
 
   export default {
     components: {
       appCounter: Counter,
+      appAnotherCounter: AnotherCounter,
       appResult: Result,
       appAnotherResult: AnotherResult,
     }
---
+++ udemy-vuejs2/vuex/src/components/AnotherCounter.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <button class="btn btn-primary" @click="increment">Increment</button>
+    <button class="btn btn-primary" @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script>
+  import { mapMutations } from "vuex";
+  export default {
+    methods: {
+      ...mapMutations([
+        'increment',
+        'decrement'
+      ])
+    }
+  }
+</script>
