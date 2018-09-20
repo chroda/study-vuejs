6ef1ef135d5a2320d93b02dea6722ff2e12485f1
chroda <chroda@chroda.com.br>
2018-09-20 16:34:39 -0300

    separetted files

--- udemy-vuejs2/vuex/src/store/store.js
+++ udemy-vuejs2/vuex/src/store/store.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import Vuex from 'vuex';
 
 import counter from './modules/counter';
+import * as actions from './actions';
+import * as mutations from './mutations';
+import * as getters from './getters';
 
 Vue.use(Vuex);
 
@@ -9,21 +12,9 @@ export const store =  new Vuex.Store({
   state: {
     value: 0
   },
-  getters: {
-    value: state => {
-      return state.value;
-    }
-  },
-  mutations: {
-    updateValue: (state, payload) => {
-      state.value = payload;
-    }
-  },
-  actions: {
-    updateValue: ({ commit }, payload) => {
-      commit('updateValue', payload);
-    }
-  },
+  getters,
+  mutations,
+  actions,
   modules: {
     counter
   }
