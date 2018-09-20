6356b6bb6ac917067dbb01fe3344d64e7c75665d
chroda <chroda@chroda.com.br>
2018-09-20 10:54:03 -0300

    actions simple

--- udemy-vuejs2/vuex/src/components/AnotherCounter.vue
+++ udemy-vuejs2/vuex/src/components/AnotherCounter.vue
@@ -1,17 +1,17 @@
 <template>
   <div>
-    <button class="btn btn-primary" @click="increment">Increment</button>
-    <button class="btn btn-primary" @click="decrement">Decrement</button>
+    <button class="btn btn-primary" @click="asyncIncrement">Async Increment</button>
+    <button class="btn btn-primary" @click="asyncDecrement">Async Decrement</button>
   </div>
 </template>
 
 <script>
-  import { mapMutations } from "vuex";
+  import { mapActions } from "vuex";
   export default {
     methods: {
-      ...mapMutations([
-        'increment',
-        'decrement'
+      ...mapActions([
+        'asyncIncrement',
+        'asyncDecrement'
       ])
     }
   }
--- udemy-vuejs2/vuex/src/store/store.js
+++ udemy-vuejs2/vuex/src/store/store.js
@@ -22,5 +22,24 @@ export const store =  new Vuex.Store({
     decrement: state => {
       state.counter--;
     }
+  },
+  actions: {
+    increment: ({ commit }) => {
+      commit('increment');
+    },
+    decrement: ({ commit }) => {
+      commit('decrement');
+    },
+    asyncIncrement: ({ commit }) => {
+      setTimeout(() => {
+        commit('increment');
+      },1000);
+    },
+    asyncDecrement: ({ commit }) => {
+      setTimeout(() => {
+        commit('decrement');
+      },1000);
+    }
   }
+
 });
