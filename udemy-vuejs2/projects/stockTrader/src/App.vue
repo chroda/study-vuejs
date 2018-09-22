f1b7bf50285d74ee53e570b1f739b131f0446d68
chroda <chroda@chroda.com.br>
2018-09-22 12:31:45 -0300

    adding a header

--- udemy-vuejs2/projects/stockTrader/src/App.vue
+++ udemy-vuejs2/projects/stockTrader/src/App.vue
@@ -1,13 +1,17 @@
 <template>
     <div class="container">
+      <app-header></app-header>
       <router-view></router-view>
     </div>
 </template>
 
 <script>
-    export default {
-
+  import Header from './components/Header.vue';
+  export default {
+    components: {
+      appHeader: Header
     }
+  }
 </script>
 
 <style>
--- udemy-vuejs2/projects/stockTrader/src/components/Header.vue
+++ udemy-vuejs2/projects/stockTrader/src/components/Header.vue
@@ -1,5 +1,32 @@
 <template>
-  <h1>
-    The Header Component
-  </h1>
+  <nav class="navbar navvar-defaut">
+    <div class="container-fluid">
+      <div class="navbar-header">
+        <router-link class="navbar-brand" to="/">Stock-Trader</router-link>
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <router-link activeClass="active" to="/portfolio"><a>Portifolio</a></router-link>
+          <router-link activeClass="active" to="/stocks"><a>Stocks</a></router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End Day</a></li>
+          <li class="dropdown">
+            <a
+              href="#"
+              class="dropdown-toggle"
+              data-toggle="dropdown"
+              role="button"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >Save & Load <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save Data</a></li>
+              <li><a href="#">Load Data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
 </template>
