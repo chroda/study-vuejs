50547e65c763048a6b6abb9c9086ea77621bcffb
chroda <chroda@chroda.com.br>
2021-10-08 14:48:43 +0100

    components

--- vue-admin/src/App.vue
+++ vue-admin/src/App.vue
@@ -1,52 +1,9 @@
 <template>
-  <header
-    class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
-  >
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"
-      >Company name</a
-    >
-    <button
-      class="navbar-toggler position-absolute d-md-none collapsed"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#sidebarMenu"
-      aria-controls="sidebarMenu"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <input
-      class="form-control form-control-dark w-100"
-      type="text"
-      placeholder="Search"
-      aria-label="Search"
-    />
-    <div class="navbar-nav">
-      <div class="nav-item text-nowrap">
-        <a class="nav-link px-3" href="#">Sign out</a>
-      </div>
-    </div>
-  </header>
+  <Nav />
 
   <div class="container-fluid">
     <div class="row">
-      <nav
-        id="sidebarMenu"
-        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-      >
-        <div class="position-sticky pt-3">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                <span data-feather="home"></span>
-                Dashboard
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
-
+      <Menu />
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="table-responsive">
           <table class="table table-striped table-sm">
@@ -74,6 +31,15 @@
     </div>
   </div>
 </template>
+
+<script>
+import Nav from "@/components/Nav";
+import Menu from "@/components/Menu";
+
+export default {
+  components: { Nav, Menu }
+};
+</script>
 
 <style>
 body {
