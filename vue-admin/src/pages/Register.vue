ca1d78a52e87b76d4b3b96b1bcf5f1bf221d54fe
chroda <chroda@chroda.com.br>
2021-10-09 17:07:46 +0100

    Redirect

--- vue-admin/src/pages/Register.vue
+++ vue-admin/src/pages/Register.vue
@@ -1,28 +1,38 @@
 <template>
   <main class="form-signin">
-    <form>
+    <form @submit.prevent="submit">
       <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
       <input
+        v-model="firstName"
         type="text"
         class="form-control"
         placeholder="First Name"
         required
       />
       <input
+        v-model="lastName"
         type="text"
         class="form-control"
         placeholder="Last Name"
         required
       />
-      <input type="email" class="form-control" placeholder="Email" required />
       <input
+        v-model="email"
+        type="email"
+        class="form-control"
+        placeholder="Email"
+        required
+      />
+      <input
+        v-model="password"
         type="password"
         class="form-control"
         placeholder="Password"
         required
       />
       <input
+        v-model="passwordConfirm"
         type="password"
         class="form-control"
         placeholder="Password Confirm"
@@ -34,13 +44,43 @@
   </main>
 </template>
 
-<script>
+<script lang="ts">
 import { ref } from "vue";
+import axios from "axios";
+import { useRouter } from "vue-router";
 
 export default {
-  name: "Register"
+  name: "Register",
 
-  // setup() {}
+  setup() {
+    const firstName = ref("");
+    const lastName = ref("");
+    const email = ref("");
+    const passworld = ref("");
+    const passworldConfirm = ref("");
+    const router = useRouter();
+
+    const submit = async () => {
+      await axios.post("http://localhost:8000/api/register", {
+        first_name: firstName.value,
+        last_name: lastName.value,
+        email: email.value,
+        passworld: passworld.value,
+        passworld_confirm: passworldConfirm.value
+      });
+
+      await router.push("/login");
+    };
+
+    return {
+      firstName,
+      lastName,
+      email,
+      passworld,
+      passworldConfirm,
+      submit
+    };
+  }
 };
 </script>
 
