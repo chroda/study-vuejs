14892a0992ac1f60f3c281322509183a03fe1211
Christian Oliveira <coliveira@quanticosolutions.com>
2023-05-23 09:43:42 +0100

    update

--- vue3-suspense/src/components/ProfileCard.vue
+++ vue3-suspense/src/components/ProfileCard.vue
@@ -9,7 +9,7 @@ const loadUserData = async () => {
         pic: "https://cdn-images-1.medium.com/fit/c/100/100/2*EcZb9cndrhTF7_d74dv2Fg.png",
         bio: "I run a VueJS community over at https://learnvue.co, develop web sites, and post whatever I find cool on the Internet.",
       });
-    }, 10000);
+    }, 1000);
   });
 };
 export default {
