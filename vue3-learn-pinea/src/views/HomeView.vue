7b539858fe6f8825793832b7f5cb4cd8f0bf03c3
Christian Marcell de Oliveira <chroda@chroda.com.br>
2024-03-05 20:21:25 +0000

    Simple Counter app

--- vue3-learn-pinea/src/views/HomeView.vue
+++ vue3-learn-pinea/src/views/HomeView.vue
@@ -1,9 +1,37 @@
+<template>
+  <div class="home">
+    <div class="count">
+      {{ count }}
+    </div>
+    <div class="buttons">
+      <button @click="decreaseCount()">-</button>
+      <button @click="increaseCount()">+</button>
+    </div>
+  </div>
+</template>
+
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from "vue";
+const count = ref(0);
+
+const increaseCount = () => {
+  count.value++;
+};
+
+const decreaseCount = () => {
+  count.value--;
+};
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+.count {
+  font-size: 60px;
+  margin: 20px;
+}
+
+.buttons button {
+  min-width: 40px;
+  font-size: 40px;
+  margin: 10px;
+}
+</style>
