90435d05d4fb2d4b2d481a3a26c1337eec0fff08
Christian Marcell de Oliveira <chroda@chroda.com.br>
2024-03-05 22:12:20 +0000

    About example

--- vue3-learn-pinea/src/views/AboutView.vue
+++ vue3-learn-pinea/src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
     <h1>About</h1>
+    <button @click="storeCounter.increaseCount" class="counter-button">
+      {{ storeCounter.count }}
+    </button>
   </div>
 </template>
+
+<script setup>
+import { useCounterStore } from "@/stores/counter";
+
+const storeCounter = useCounterStore();
+</script>
+
+<style>
+.counter-button {
+  font-size: 50px;
+}
+</style>
